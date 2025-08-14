74c8189623c4b48798a272832be43054ed06aafc
brenokuro <brenosla4@gmail.com>
2025-08-14 19:57:45 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // Função para calcular a data de entrega estimada
 function calcularDataEntrega(anoAtual, prazoEntregaDias) {
-    // Cria um objeto Date com o ano atual
-    const dataEntrega = new Date(anoAtual, 0, 1); // Começa no primeiro dia do ano
+   
+    const dataEntrega = new Date(anoAtual, 0, 1); 
 
     //prazo de entrega em dias
     dataEntrega.setDate(dataEntrega.getDate() + prazoEntregaDias);
 
-    // para formatar a  data para o formato DD/MM/AAAA
     const dia = String(dataEntrega.getDate()).padStart(2, '0');
     const mes = String(dataEntrega.getMonth() + 1).padStart(2, '0'); 
     const ano = dataEntrega.getFullYear();
@@ -14,7 +13,7 @@ function calcularDataEntrega(anoAtual, prazoEntregaDias) {
     return `${dia}/${mes}/${ano}`;
 }
 // teste
-const anoAtual = 2023; // Ano atual
-const prazoEntregaDias = 15; // Prazo de entrega em dias
+const anoAtual = 2023; 
+const prazoEntregaDias = 15; 
 const dataEstimativaEntrega = calcularDataEntrega(anoAtual, prazoEntregaDias);
 console.log(`A data estimada de entrega é: ${dataEstimativaEntrega}`); 
